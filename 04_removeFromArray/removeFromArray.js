0b3ecc7c6ece1487a6982740cf01e08137fb1ebf
Maharta <gedemaharta.dev@gmail.com>
2022-01-08 20:13:54 +0800

    finished the fourth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...args) {
+  args.forEach((arg) => {
+    for (let i = 0; i < arr.length; i++) {
+      if (arg === arr[i]) {
+        arr.splice(i, 1);
+        i--;
+      }
+    }
+  });
+  return arr;
 };
 
 // Do not edit below this line
