19b68df58ed99f1f816d390febd5c86b74f83d8c
Maharta <gedemaharta.dev@gmail.com>
2022-01-08 15:57:10 +0800

    completed second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if(num === 0) return '';
+    if(num < 0) return 'ERROR';
 
+    let initialString = string;
+    for(let i = 1; i<num; i ++){
+        string += initialString; 
+    }
+    return string;
 };
 
 // Do not edit below this line
