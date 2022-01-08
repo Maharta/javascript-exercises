fa2a1619dda75912a9de6e50d230b7ead3ef9b89
Maharta <gedemaharta.dev@gmail.com>
2022-01-08 20:46:00 +0800

    finished the fifth exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if(start > end) {
+        [start, end] = [end, start];
+    }
 
+    if (start < 0 || end < 0 || typeof start !== 'number' || typeof end !== 'number') return 'ERROR';
+
+    const n = start - 1;
+    const finalValue = (end - n) * (end + start) / 2;
+    return finalValue;
 };
 
 // Do not edit below this line
