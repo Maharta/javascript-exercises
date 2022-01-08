7f4d6ea813115195de31741e3c19b79713ac58c1
Maharta <gedemaharta.dev@gmail.com>
2022-01-08 16:14:28 +0800

    finished exercise no 2

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
 const reverseString = function(str) {
+    const stringArray = str.split("");
+    stringArray.reverse();
+    return stringArray.join("");
+    
 
 };
+
+console.log(reverseString('Aku adalah Kapiten'));
 
 // Do not edit below this line
 module.exports = reverseString;
