b1ec409dadf51f8f3a3ec4a28dd1397fc6c50a60
Maharta <gedemaharta.dev@gmail.com>
2022-01-08 21:25:35 +0800

    finished seventh exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,12 @@
-const ftoc = function() {
+const ftoc = function(value) {
+  value = (value - 32) * 5/9;
+  return Math.round(value * 10) / 10;
 
 };
 
-const ctof = function() {
+const ctof = function(value) {
+  value = value * 9/5 + 32;
+  return Math.round(value * 10) / 10
 
 };
 
